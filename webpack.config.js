a8802eb7e1b0d4c51ce477a4a73ced940979b2f6
andy-a-d-nguyen <andy-a-d-nguyen@zoho.com>
2020-11-12 15:40:55 -0800

    installed and set up eslint

--- webpack.config.js
+++ webpack.config.js
@@ -8,21 +8,21 @@ module.exports = {
   },
   module: {
     rules: [
-    {
-      test: /\.m?(js|jsx)$/,
-      exclude: /(node_modules|bower_components)/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react']
-        }
-      }
-    }
-    ]
+      {
+        test: /\.m?(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
   },
   mode: 'development',
-  watch: true
+  watch: true,
 };
