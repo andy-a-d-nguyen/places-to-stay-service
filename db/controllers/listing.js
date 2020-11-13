26dfdb4f02ea3fb8ad43e4e50a6339034469d693
andy-a-d-nguyen <andy-a-d-nguyen@zoho.com>
2020-11-12 21:11:27 -0800

    created db, schema and inserted mock data

---
+++ db/controllers/listing.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const listingSchema = mongoose.Schema({
+  listingID: { type: Number, unique: true },
+  listingName: String,
+  morePlacesID1: Number,
+  morePlacesID2: Number,
+  morePlacesID3: Number,
+  morePlacesID4: Number,
+  morePlacesID5: Number,
+  morePlacesID6: Number,
+  morePlacesID7: Number,
+  morePlacesID8: Number,
+  morePlacesID9: Number,
+  morePlacesID10: Number,
+  morePlacesID11: Number,
+  morePlacesID12: Number,
+});
+
+const listingModel = mongoose.model('Listing', listingSchema);
+
+module.exports.listingModel = listingModel;
