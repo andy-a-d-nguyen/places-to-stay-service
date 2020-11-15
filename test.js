2dfbc396d71665958cbc1224616a174ee460f6f3
andy-a-d-nguyen <andy-a-d-nguyen@zoho.com>
2020-11-14 18:07:31 -0800

    tested relevant mock data insert operation and passed

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const {MongoClient} = require('mongodb');
+const { MongoClient } = require('mongodb');
 
 describe('insert', () => {
   let connection;
@@ -17,12 +17,16 @@ describe('insert', () => {
   });
 
   it('should insert a doc into collection', async () => {
-    const users = db.collection('users');
+    const listings = db.collection('listings');
 
-    const mockUser = { _id: 'some-user-id', name: 'John' };
-    await users.insertOne(mockUser);
+    const mockListing = {
+      listingID: 1,
+      listingName: 'mockListing',
+      morePlacesID: [],
+    };
+    await listings.insertOne(mockListing);
 
-    const insertedUser = await users.findOne({ _id: 'some-user-id' });
-    expect(insertedUser).toEqual(mockUser);
+    const insertedListing = await listings.findOne({ listingID: 1 });
+    expect(insertedListing).toEqual(mockListing);
   });
 });
